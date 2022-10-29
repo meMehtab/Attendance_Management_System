2b6d892024433882361076e252c8882af0fc8041
meMehtab <mehtabbir2@gmail.com>
2022-10-29 14:14:24 +0530

    Added Logout and Corrected Signup,login And Password retrieval

--- attendance_management_system.cpp
+++ attendance_management_system.cpp
@@ -12,13 +12,13 @@ class profile
     public:
         // int employee_ID;
         // static int no_of_employees;                                      //to count number of id's already made
-        string fullName;
-        string email;
-        string password;
-        string userName;  
-        string mobileNo;
-        string secQue;
-        string secAns;
+        char fullName[30];
+        char email[30];
+        char password[30];
+        char userName[30];  
+        char mobileNo[30];
+        char secQue[30];
+        char secAns[30];
     
         // profile()
         // {
@@ -35,11 +35,11 @@ class profile
         // }
 };
 
-// void getinput(char input[30])
-// {   
-//     fgets(input, 30, stdin);
-//     input[strlen(input) - 1] = 0;
-// }
+void getinput(char input[30])
+{   
+    fgets(input, 30, stdin);
+    input[strlen(input) - 1] = 0;
+}
 
 
 // void getusername(char input[30],profile &object,FILE *datafile)
@@ -84,181 +84,233 @@ class profile
 int main()
 {
      int selection;
+
     FILE *datafile;
-    //profile user;
+    fstream file_obj; 
+
+    profile user;
+
+    char nouse[30] = "User_";
     char username2[30];
     char password2[30];
+    char password3[30];
     char username3[30];
+    char username4[30];
     char secAns2[30];
     cout<<"\t\t\t****Login System***\n";
     cout<<"Choose your option from below:\n";
     cout<<"1.Signup\n";
     cout<<"2.Login\n";
-    cout<<"1.Logout\n";
+    cout<<"3.Logout\n";
     cout<<"4.Forgot Password\n";
+    cout<<"5.Exit\n";
     cout<<"Enter the serial number of your choice:\n";
     cin>>selection;
     fgetc(stdin);
-    
-    switch(selection){
-        case 1:
-                system("cls");
-                printf("\t\t\t***Signup Portal***\n");
-                printf("Please enter the required information:\n");
 
-                profile user;
-                ofstream file_obj;                                                  // Object to write in file
+        switch(selection){
+                case 1:
+                        system("cls");
+                        printf("\t\t\t***Signup Portal***\n");
+                        printf("Please enter the required information:\n");
 
-                //int number_of_ids = user.getEmployeeID();
-                // string filename = "User" + to_string(number_of_ids) + ".txt";
+                        //profile user;
+                        // ofstream file_obj;                                                  // Object to write in file
 
-                // file_obj.open(filename, ios::app);
+                        printf("Enter your full name:\n");
+                        getinput(user.fullName);
+                        printf("Please enter your username:\n");
+                        //getusername(user.userName,user,datafile);
+                        getinput(user.userName);
 
-                // printf("Enter your full name:\n");
-                // getinput(user.fullName);
-                // printf("Please enter your username:\n");
-                // getusername(user.userName,user,datafile);
-                // printf("Please enter your email:\n");
-                // getinput(user.email);
-                // printf("Please enter your mobile no.:\n");
-                // getinput(user.mobileNo);
-                // printf("Please enter your password:\n");
-                // getinput(user.password);
-                // printf("Enter the security question:\n");
-                // getinput(user.secQue);
-                // printf("Enter the answer to security question:\n");
-                // getinput(user.secAns);
+                        //const char* demo = user.userName.c_str();
+                        //string defaultstr = to_string(user.userName);                             //to convert const char to string
+                        // char nouse[30] = "User_";
+                        strcat(nouse,user.userName);
+                        // string filename = "User_" + defaultstr;
+                        file_obj.open(nouse, ios::app);
 
-                printf("Enter your full name:\n");
-                getline(cin,user.fullName);
+                        printf("Please enter your email:\n");
+                        getinput(user.email);
+                        printf("Please enter your mobile no.:\n");
+                        getinput(user.mobileNo);
+                        printf("Please enter your password:\n");
+                        getinput(user.password);
+                        printf("Enter the security question:\n");
+                        getinput(user.secQue);
+                        printf("Enter the answer to security question:\n");
+                        getinput(user.secAns);
 
-                string filename = "User_" + user.fullName;
-                file_obj.open(filename, ios::app);
+                        system("cls");
+                        printf("Creating Account.......please wait\n");
 
-                printf("Please enter your username:\n");
-                //getusername(user.userName,user,datafile);
-                getline(cin,user.userName);
-                printf("Please enter your email:\n");
-                getline(cin,user.email);
-                printf("Please enter your mobile no.:\n");
-                getline(cin,user.mobileNo);
-                printf("Please enter your password:\n");
-                getline(cin,user.password);
-                printf("Enter the security question:\n");
-                getline(cin,user.secQue);
-                printf("Enter the answer to security question:\n");
-                getline(cin,user.secAns);
+                        //datafile = fopen("Users.html","a+");
+                        // fwrite(&user, sizeof(), 1, datafile);
 
-                system("cls");
-                printf("Creating Account.......please wait\n");
+                        file_obj.write((char*)&user, sizeof(user));
 
-                //datafile = fopen("Users.html","a+");
-                // fwrite(&user, sizeof(), 1, datafile);
+                        if(fwrite != 0){
+                        printf("\033[0;32m");
+                        cout<<"Your account has been created."<<endl;
+                        cout<<"Your username is : "<<user.userName<<endl;
+                        printf("\033[0m");
+                        }
+                        else
+                        {
+                        cout<<"There was an error."<<endl;
+                        }
+                        // fclose(datafile);
+                        break;
 
-                file_obj.write((char*)&user, sizeof(user));
 
-                // if(fwrite != 0){
-                //     printf("\033[0;32m");
-                //     printf("Your account has been created.\n");
-                //     printf("Your username is: %s\n",user.userName);
-                //     printf("\033[0m");
-                // }
-                // else
-                // {
-                //     printf("There was an error.\n");
-                // }
-                // fclose(datafile);
-                break;
+                case 2:   
+                        system("cls");
+                        printf("\t\t\t***Login Portal***\n");   
+                        printf("Enter your username:\n");
+                        getinput(username2);
+                        printf("Enter your password:\n");
+                        getinput(password2);
 
-        // case 2:   
-        //         system("cls");
-        //         printf("\t\t\t***Login Portal***\n");   
-        //         printf("Enter your username:\n");
-        //         getinput(username2);
-        //         printf("Enter your password:\n");
-        //         getinput(password2);
+                        system("cls");
 
-        //         system("cls");
-
-        //         datafile = fopen("Users.html","r");
-        //         while(fread(&user, sizeof(struct data), 1, datafile))
-        //         {
-        //             if(!strcmp(user.userName,username2)){
-        //                 if(!strcmp(user.password,password2)){
-                            
-        //                     printf("\033[0;32m");
-        //                     printf("Successful login at %s\n\n",user.fullName);
-        //                     printf("\033[0m");
-
-        //                     printf("Your profile data is:\n");
-        //                     printf("Full Name       : %s\n",user.fullName);
-        //                     printf("Email           : %s\n",user.email);
-        //                     printf("Username        : %s\n",user.userName);
-        //                     printf("Contact details : %s\n",user.mobileNo);
-        //                     printf("Password        : %s\n",user.password);
-                            
-        //                 }
-        //                 else
-        //                 {   
-        //                     printf("\033[0;31m");
-        //                     printf("Enter the correct password!\n");
-        //                     printf("\033[0m");
-        //                 }
+                        //profile user2;
+                        // fstream file_obje;
                         
-        //             }
-        //             else
-        //             {   
-        //                 printf("\033[0;31m");
-        //                 printf("User not registered.\n");
-        //                 printf("\033[0m");
-        //             }
-        //         }
-        //         fclose(datafile);
-        //         break;
+                        strcat(nouse,username2);
 
-        // case 3:
-        //         break;
+                        // string demostr = to_string(username2);
+                        // string filename2 = "User_" + demostr;
+                        //const char* str = filename2.c_str();
 
-        // case 4: 
-        //         system("cls");
-        //         printf("\t\t\t***Password retrieval Portal***\n");
-                
-        //         printf("Enter your username:\n");
-        //         getinput(username3);
+                        // file_obje.open(filename2, ios::in);
 
-        //         datafile = fopen("Users.html","r");
-        //         while(fread(&user, sizeof(struct data), 1, datafile))
-        //         {   
-        //             if(!strcmp(user.userName,username3)){
-        //             printf("Answer the following Security Question:\n");
-        //             printf("%s\n",user.secQue);
-        //             getinput(secAns2);
-        //             if(!strcmp(user.secAns,secAns2)){
-        //             system("cls");
-        //             printf("retrieving.......password\n");
-        //             printf("\033[0;32m");
-        //             printf("Success\n");
-        //             printf("\033[0m");
-        //             printf("Your password was : %s\n",user.password);
-        //             }else
-        //             {
-        //                 system("cls");
-        //                 printf("\033[0;31m");
-        //                 printf("Incorrect answer!\n");
-        //                 printf("\033[0m");
-        //             }
-        //             }
-        //             else
-        //             {
-        //                 system("cls");
-        //                 printf("\033[0;31m");
-        //                 printf("No such user Found!\n");
-        //                 printf("\033[0m");
-        //             }
-        //         }
+                        // if (!file_obje) {
+                        //         cout << "No such User Registered"<<endl;
+                        // }
+                        // else{
+                        datafile = fopen(nouse,"r");
+                        while(fread(&user, sizeof(user), 1, datafile))
+                        {
+                        if(!strcmp(user.userName,username2)){
+                                if(!strcmp(user.password,password2)){
+                                
+                                printf("\033[0;32m");
+                                printf("Successful login at %s\n\n",user.fullName);
+                                printf("\033[0m");
 
-        //         fclose(datafile);
-        //         break;
-                 }
-        return 0;
+                                printf("Your profile data is:\n");
+                                printf("Full Name       : %s\n",user.fullName);
+                                printf("Email           : %s\n",user.email);
+                                printf("Username        : %s\n",user.userName);
+                                printf("Contact details : %s\n",user.mobileNo);
+                                printf("Password        : %s\n",user.password);
+                                
+                                }
+                                else
+                                {   
+                                printf("\033[0;31m");
+                                printf("Enter the correct password!\n");
+                                printf("\033[0m");
+                                }
+                                
+                        }
+                        else
+                        {   
+                                printf("\033[0;31m");
+                                printf("User not registered.\n");
+                                printf("\033[0m");
+                        }
+                        }
+                        fclose(datafile);
+                        //}
+                        break;
+
+                case 3: 
+                        system("cls");        
+                        printf("\t\t\t***Logout Portal***\n");
+                        
+                        printf("Enter your username:\n");
+                        getinput(username4);
+                        printf("Enter your password:\n");
+                        getinput(password3);
+
+                        //system("cls");
+
+                        strcat(nouse,username4);
+                        datafile = fopen(nouse,"r");
+
+                        while(fread(&user, sizeof(user), 1, datafile))
+                        {
+                        if(!strcmp(user.userName,username4)){
+                                if(!strcmp(user.password,password3)){
+                                
+                                printf("\033[0;32m");
+                                printf("Successful logout at %s\n\n",user.fullName);
+                                printf("\033[0m");                            
+                                }
+                                else
+                                {   
+                                printf("\033[0;31m");
+                                printf("Enter the correct password!\n");
+                                printf("\033[0m");
+                                }
+                                
+                        }
+                        else
+                        {   
+                                printf("\033[0;31m");
+                                printf("User not registered.\n");
+                                printf("\033[0m");
+                        }
+                        }
+                        fclose(datafile);
+
+                        break;
+
+                case 4: 
+                        system("cls");
+                        printf("\t\t\t***Password retrieval Portal***\n");
+                        
+                        printf("Enter your username:\n");
+                        getinput(username3);
+
+                        strcat(nouse,username3);
+
+                        datafile = fopen(nouse,"r");
+                        while(fread(&user, sizeof(user), 1, datafile))
+                        {   
+                        if(!strcmp(user.userName,username3))
+                        {
+                                printf("Answer the following Security Question:\n");
+                                printf("%s\n",user.secQue);
+                                getinput(secAns2);
+                                if(!strcmp(user.secAns,secAns2)){
+                                        system("cls");
+                                        printf("retrieving.......password\n");
+                                        printf("\033[0;32m");
+                                        printf("Success\n");
+                                        printf("\033[0m");
+                                        printf("Your password was : %s\n",user.password);
+                                }else
+                                        {
+                                                system("cls");
+                                                printf("\033[0;31m");
+                                                printf("Incorrect answer!\n");
+                                                printf("\033[0m");
+                                        }
+                        }
+                        else
+                        {
+                                system("cls");
+                                printf("\033[0;31m");
+                                printf("No such user Found!\n");
+                                printf("\033[0m");
+                        }
+                        }
+
+                        fclose(datafile);
+                        break;
+                        // }
+                return 0;
+        }
 }
